2ad8047eab3cbe6bc18010c2df73c9a55073a845
vyasgupta <vgupta13@terpmail.umd.edu>
2020-08-15 14:19:38 -0400

    finished helper functions, debugged and finished callable function

--- util.py
+++ util.py
@@ -5,18 +5,31 @@ from fbprophet import Prophet
 from fbprophet.plot import plot_plotly, plot_components_plotly
 import matplotlib.pyplot as plt
 import scipy.stats as stats
-import plotly.offline as py
+import plotly.express as px
+import calendar
 
 def gen_analysis(states):
-    """
+    """ Calls all the helper functions and returns the dictionary containing all the figures. Use 'fbprophet' to get a
+    tuple of predictions and components, 'top10' to get a bar chart of top 10 historical months by damage, and
+    'month_dist' to get a bar chart of distribution of damage by month.
+    
     Args:
-        states ([type]): [description]
+        states (list of str): List of states to analyze. Assumed to all be fully capitalized.
 
     Returns:
-        [type]: [description]
+        dict of str to plotly graphs: contains all the relevant plotly graphs
     """
 
-    return None
+    ret = {} # dictionary to hold all the graphs
+    df = _get_df(states)
+
+    # use these 
+    ret['fbprophet'] = _gen_fbprophet_model(df, use_outliers=True)
+    ret['top10'] = _significant_months_barchart(df, metric='damage')
+    ret['month_dist'] = _monthly_dist(df, metric='damage')
+
+    return ret
+
 
 d = {'K': 3, 'k': 3, 'M': 6, 'm': 6, 'B': 9, 'b': 9}
 def _str_to_num(s):
@@ -40,6 +53,7 @@ def _str_to_num(s):
         return float(s)
     else: # otherwise, we just return
         return s
+
 
 def _get_df(states):
     """Queries data from the 'data' subdirectory where the state of the row is in the list of states provided as a 
@@ -93,6 +107,7 @@ def _get_df(states):
     # of individual data points)
     return df.drop(columns=['STATE', 'INJURIES_DIRECT','INJURIES_INDIRECT','DEATHS_DIRECT','DEATHS_INDIRECT']).resample('M').sum()
 
+
 def _gen_fbprophet_model(df, use_outliers=True):
     """Using the Prophet package from Facebook, generate two interactive plotly graphs projecting damage for the next 
     56 months (i.e. until 2025). First graph is the explicit forecast, the second graph contains the trend and 
@@ -103,7 +118,7 @@ def _gen_fbprophet_model(df, use_outliers=True):
         use_outliers(Boolean): whether or not to filter out outliers before analysis
 
     Return:
-        plotly figures: two plotly figures for forecasting and components of forecast
+        tuple of plotly figures: two plotly figures for forecasting and components of forecast
     """
 
     series = df['DAMAGE'] # select the damage series to analyze
@@ -119,22 +134,88 @@ def _gen_fbprophet_model(df, use_outliers=True):
     m = Prophet()
     m.fit(temp)
 
-    # create a future dataframe to predict the next 36 months
-    future = m.make_future_dataframe(periods=36, freq='M')
+    # create a future dataframe to predict the next 56 months
+    future = m.make_future_dataframe(periods=56, freq='M')
 
     # make the prediction
     forecast = m.predict(future)
 
     # generate the two ploty figures, first showing forecast, second showing forecast components
     fig = plot_plotly(m, forecast)
+    fig.update_layout(title="Forecasting Damage over the next 56 Months")
     fig2 = plot_components_plotly(m, forecast)
+    fig2.update_layout(title='Forecast Components - Trend & Seasonality')
 
     return fig, fig2
 
-def _significant_months_barchart(df):
 
-    return None
+def _significant_months_barchart(df, metric='damage'):
+    """Determines the ten months with the most deaths, injuries or damage from the dataframe given. Uses the metric
+    provided to rank months. Generates an ordered bar chart using these ten months.
 
-def _monthly_cost_hist(df):
+    Args:
+        df (pandas.DataFrame): DataFrame containing monthly sums to analyze
+        metric (str, optional): Metric to analyze. Can be 'damage', 'injuries' or 'deaths'. Defaults to 'damage'.
 
-    return None
+    Returns:
+        plotly figure: barchart generated
+    """
+
+    # check for erroneous input
+    if not metric in ['damage', 'injuries', 'deaths']:
+        raise ValueError('Metric must be one of: damage, injuries, deaths')
+    elif metric == 'damage': 
+        # next few else ifs set the title
+        title = 'Costliest Months in History by Damage'
+    elif metric == 'deaths':
+        title = 'Months in History with Most Deaths caused by NOAA Events'
+    else:
+        title = 'Months in History with Most Injuries caused by NOAA Events'
+
+    series = df[metric.upper()].nlargest(10) # for the given metric, get the 10 largest values
+
+    # create dataframe for plotting
+    data = pd.DataFrame()
+    data['Month'] = series.index.strftime("%B %Y") # reformat datetime as MonthName Year
+    data['Year'] = series.index.strftime("%Y") # to color by year
+    data[metric.upper()] = series.values
+
+    # generate figure using color code
+    fig = px.bar(data, x='Month', y=metric.upper(), color='Year', title=title) 
+    fig.update_layout(xaxis_categoryorder = 'total descending') # ensure it is descending order
+
+    return fig
+
+
+def _monthly_dist(df, metric='damage'):
+    """Generates the distribution of damage, injuries or deaths by month and produces a plotly bar chart
+
+    Args:
+        df (pandas.DataFrame): DataFrame containing monthly sums to analyze
+        metric (str, optional): Metric to analyze. Can be 'damage', 'injuries' or 'deaths'. Defaults to 'damage'.
+
+    Returns:
+        plotly figure: barchart generated
+    """
+
+    if not metric in ['damage', 'injuries', 'deaths']:
+        raise ValueError('Metric must be one of: damage, injuries, deaths')
+    elif metric == 'damage': 
+        # next few else ifs set the title
+        title = 'Distribution of Damage over Months'
+    elif metric == 'deaths':
+        title = 'Distribution of Deaths over Months'
+    else:
+        title = 'Distribution of Injuries over Months'
+
+    series = df[metric.upper()]
+
+    series = series.groupby(series.index.month).sum()
+    series.index = map(lambda m: calendar.month_name[m], series.index)
+
+    # create dataframe for plotting
+    data = pd.DataFrame()
+    data['Month'] = series.index
+    data[metric.upper()] = series.values
+
+    return px.bar(data, x='Month', y=metric.upper(), title=title)
